c2fb405e94f8d3d84923831dae22932b10b64a0b
FrontRowWithJ <adebusum@tcd.ie>
2022-10-03 11:47:53 +0100

    Revert "First commit"
    This reverts commit ffe45e81488e07fea000d0224b6aaca276ee160c.

--- Semester 1/Machine Learning/Logistic Regression/example.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-Xtrain = np.random.uniform(0, 1, 100)
-ytrain = np.sign(Xtrain - 0.5)
-Xtrain = Xtrain.reshape(-1, 1)
-model = LogisticRegression(penalty="none", solver="lbfgs")
-model.fit(Xtrain, ytrain)
-print("intercept %f, slope %f" % (model.intercept_, model.coef_))
-print("y = %fx %s %f" % (model.coef_, "+" if model.intercept_ >= 0 else "-",  abs(model.intercept_)))
--- Semester 1/Machine Learning/Logistic Regression/plotting_example.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-plt.rc("font", size=18)
-plt.rcParams["figure.constrained_layout.use"] = True
-plt.scatter(Xtrain, ytrain, color="red", marker="+")
-plt.scatter(Xtrain, ypred, color="green", marker="+")
-plt.xlabel("input x")
-plt.ylabel("output y")
-plt.legend(["train", "predict"])
-plt.show()
